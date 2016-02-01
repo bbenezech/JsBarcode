e6ec42cbc5b407a9421a757798060e804c46f8bc
Benoit Benezech <benoit.benezech@gmail.com>
2016-02-01 17:11:38 +0100

    remove all the useless junk

--- JsBarcode.js
+++ JsBarcode.js
@@ -1,12 +1,5 @@
-(function($){
-
-	JsBarcode = function(image, content, options, validFunction) {
-
-		//Check if the image parameter should be
-		if(typeof image === "string"){
-			image = document.querySelector(image);
-		}
-
+(function(){
+	JsBarcode = function(content, options, validFunction) {
 		var merge = function(m1, m2) {
 			var newMerge = {};
 			for (var k in m1) {
@@ -18,80 +11,86 @@
 			return newMerge;
 		};
 
-		//This tries to call the valid function only if it's specified. Otherwise nothing happens
-		var validFunctionIfExist = function(valid){
-		    if(validFunction){
-		        validFunction(valid);
-		    }
-		};
-
 		//Merge the user options with the default
-		options = merge(JsBarcode.defaults, options);
+		options = merge({
+			title: null,
+			titleAlign: 'left',
+			width:	2,
+			height:	100,
+			quite: 10,
+			format:	'CODE128',
+			displayValue: false,
+			fontOptions: '',
+			font:'monospace',
+			textAlign:'center',
+			textPadding:0,
+			fontSize: 12,
+			backgroundColor:'',
+			lineColor:'#000'
+		}, options);
 
-		//Create the canvas where the barcode will be drawn on
-		// Check if the given image is already a canvas
-		var canvas = image;
-
-		// check if it is a jQuery object
-		if ($ && canvas instanceof $) {
-			// get the DOM element of the object
-			canvas = image.get(0);
-		}
-
-		// check if DOM element is a canvas, otherwise it will be probably an image so create a canvas
-		if (!(canvas instanceof HTMLCanvasElement)) {
-			canvas = document.createElement('canvas');
-		}
-
-		//Abort if the browser does not support HTML5canvas
-		if (!canvas.getContext) {
-			return image;
-		}
+		const canvas = document.createElement('canvas');
 
 		var encoder = new window[options.format](content);
-
-		//Abort if the barcode format does not support the content
-		if(!encoder.valid()){
-		    validFunctionIfExist(false);
-			throw new Error('The data is not valid for the type of barcode.');
-		}
 
 		//Encode the content
 		var binary = encoder.encoded();
 
-		var _drawBarcodeText = function (text) {
-					var x, y;
+		var _drawBarcodeText = function (text, y) {
+			var x;
 
-					y = options.height + options.textPadding;
+			ctx.font = options.fontOptions + ' ' + options.fontSize + 'px ' + options.font;
+			ctx.textBaseline = 'top';
 
-					ctx.font = options.fontOptions + " " + options.fontSize + "px "+options.font;
-					ctx.textBaseline = "bottom";
-					ctx.textBaseline = 'top';
+			if(options.textAlign == 'left'){
+				x = options.quite;
+				ctx.textAlign = 'left';
+			}
+			else if(options.textAlign == 'right'){
+				x = canvas.width - options.quite;
+				ctx.textAlign = 'right';
+			}
+			else{ //All other center
+				x = canvas.width / 2;
+				ctx.textAlign = 'center';
+			}
 
-					if(options.textAlign == "left"){
-						x = options.quite;
-						ctx.textAlign = 'left';
-					}
-					else if(options.textAlign == "right"){
-						x = canvas.width - options.quite;
-						ctx.textAlign = 'right';
-					}
-					else{ //All other center
-						x = canvas.width / 2;
-						ctx.textAlign = 'center';
-					}
+			ctx.fillText(text, x, y);
+		}
 
-					ctx.fillText(text, x, y);
-				}
+		var _drawTitle = function (text, y) {
+			var x;
+
+			if(options.titleAlign == 'left'){
+				x = options.quite;
+				ctx.textAlign = 'left';
+			}
+			else if(options.titleAlign == 'right'){
+				x = canvas.width - options.quite;
+				ctx.textAlign = 'right';
+			}
+			else{ //All other center
+				x = canvas.width / 2;
+				ctx.textAlign = 'center';
+			}
+
+			ctx.font = options.fontOptions + " " + options.fontSize + "px "+options.font;
+			ctx.textBaseline = 'top';
+
+			ctx.fillText(text, x, y);
+		}
 
 		//Get the canvas context
 		var ctx	= canvas.getContext("2d");
 
 		//Set the width and height of the barcode
 		canvas.width = binary.length*options.width+2*options.quite;
-        //Set extra height if the value is displayed under the barcode. Multiplication with 1.3 t0 ensure that some
-        //characters are not cut in half
-		canvas.height = options.height + (options.displayValue ? options.fontSize * 1.3 : 0) + options.textPadding;
+    //Set extra height if the value is displayed under the barcode. Multiplication with 1.3 t0 ensure that some
+    //characters are not cut in half
+		canvas.height =
+			options.height +
+			(options.displayValue ? options.fontSize + options.textPadding : 0) +
+			(options.title ? options.fontSize + options.textPadding : 0);
 
 		//Paint the canvas
 		ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -100,59 +99,29 @@
 			ctx.fillRect(0,0,canvas.width,canvas.height);
 		}
 
+		var currentY = 0;
+
+		if(options.title){
+			_drawTitle(options.title, currentY);
+			currentY += options.fontSize + options.textPadding;
+		}
+
 		//Creates the barcode out of the encoded binary
 		ctx.fillStyle = options.lineColor;
 		for(var i=0;i<binary.length;i++){
 			var x = i*options.width+options.quite;
 			if(binary[i] == "1"){
-				ctx.fillRect(x,0,options.width,options.height);
+				ctx.fillRect(x,currentY,options.width,options.height);
 			}
 		}
+		currentY += options.height;
 
 		if(options.displayValue){
-			_drawBarcodeText(encoder.getText());
+			currentY += options.textPadding;
+			_drawBarcodeText(encoder.getText(), currentY);
 		}
 
 		//Grab the dataUri from the canvas
-		uri = canvas.toDataURL('image/png');
-
-		// check if given image is a jQuery object
-		if ($ && image instanceof $) {
-			// check if DOM element of jQuery selection is not a canvas, so assume that it is an image
-			if (!(image.get(0) instanceof HTMLCanvasElement)) {
-				 //Put the data uri into the image
-			 	image.attr("src", uri);
-			}
-		} else if (!(image instanceof HTMLCanvasElement)) {
-			// There is no jQuery object so just check if the given image was a canvas, if not set the source attr
-			image.setAttribute("src", uri);
-		}
-
-
-		validFunctionIfExist(true);
-
+		return canvas.toDataURL('image/png');
 	};
-
-	JsBarcode.defaults = {
-		width:	2,
-		height:	100,
-		quite: 10,
-		format:	"CODE128",
-		displayValue: false,
-		fontOptions: "",
-		font:"monospace",
-		textAlign:"center",
-		textPadding:0,
-		fontSize: 12,
-		backgroundColor:"",
-		lineColor:"#000"
-	};
-
-	if ($) {
-		$.fn.JsBarcode = function(content, options,validFunction){
-			JsBarcode(this, content, options,validFunction);
-			return this;
-		};
-	}
-
-})(typeof jQuery != 'undefined' ? jQuery : null);
+})();
--- README.md
+++ README.md
@@ -1,122 +1,29 @@
-Demo
-----
-[Barcode Generator](http://lindell.github.io/JsBarcode/)
+Forked from @lindell. Added title, removed all the jQuery/automatic image-linking crap, removed all the useless encodings/examples. Have fun.
 
-Introduction
-----
-JsBarcode is a simple way to create different types of 1d barcodes.  
-The plugin uses Html5Canvas to generate draw the barcodes
-
-#### This is the list of supported barcodes:
-*  CODE128 (B or C)
-*  EAN (13 and 8)
-*  UPC-A
-*  CODE39
-*  ITF (Interleaved 2 of 5)
-*  ITF14
-*  Pharmacode
-
-Examples:
-----
-
-#### First create an image (or canvas)
-````html
-<img id="barcode">
-````
-
-#### Code:
-````javascript
-$("#barcode").JsBarcode("9780199532179",{format:"EAN", displayValue:true, fontSize:20});
-````
-##### Result:
-![Result](http://lindell.github.io/JsBarcode/README_images/ean.png)
-
-
-
-#### Code:
-````javascript
-$("#barcode").JsBarcode("Hi!");
-````
-
-##### Result:
-![Result](http://lindell.github.io/JsBarcode/README_images/hi.png)
-
-
-
-#### Code (without jQuery):
-````javascript
-JsBarcode("#barcode","JsBarcode is easy!",{width:1,height:25});
-````
-##### Result:
-![Result](http://lindell.github.io/JsBarcode/README_images/javascript_is_fun.png)
-
-Setup:
-----
-Download and include the [comined script](https://github.com/lindell/JsBarcode/releases) that contain everything you need.
-
-````
-<script src="JsBarcode.all.min.js"></script>
-````
-
-Bower:
-----
-As well as downloading the files and including them regularly,
-you can use [Bower](http://bower.io) to install and manage the library
-````
-bower install jsbarcode --save
-````
-
-Use
-----
-#### There are two ways of using the library:
-With jQuery:
-````javascript
-$(object).JsBarcode(string,options);
-````
-Or pure JavaScript:
-````javascript
-JsBarcode(object, string, options);
-````
-
-#### The parameters:
-*  string is the sring to be encoded to the barcode
-*  options is additional options put i an object (look below)
-
-#### The default options:
-````javascript
-{
-	width:	2,
-	height:	100,
-	quite: 10,
-	format:	"CODE128",
-	displayValue: false,
-	fontOptions: "",
-	font:"monospace",
-	textAlign:"center",
-	textPadding:0,
-	fontSize: 12,
-	backgroundColor:"",
-	lineColor:"#000"
-}
-````
-
-
-
-
-
-
-Minify the latest code
-----
-Use the [closure compiler](http://closure-compiler.appspot.com/home) with this input
-````
-// ==ClosureCompiler==
-// @output_file_name JsBarcode.all.min.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/CODE128.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/CODE39.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/EAN_UPC.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/ITF.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/ITF14.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/pharmacode.js
-// @code_url https://raw.github.com/lindell/JsBarcode/master/JsBarcode.js
-// ==/ClosureCompiler==
-````
+```html
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>JsBarcode Example</title>
+	<script src="../CODE128.js"></script>
+	<script src="../JsBarcode.js"></script>
+</head>
+<body>
+	<div>
+		<img id="barcode"/>
+		<script>
+			document.getElementById('barcode').src = JsBarcode('10003591671', {
+				title: 'N° RPPS',
+				titleAlign: 'left',
+			  width:  2,
+			  height: 85,
+			  displayValue: true,
+			  fontSize: 20,
+			  textPadding: 2,
+			});
+		</script>
+	</div>
+</body>
+</html>
+```
